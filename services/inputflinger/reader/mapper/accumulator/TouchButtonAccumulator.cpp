e172b5f860fddae75d9aedfee92a0941da6e3484
Erfan Abdi <erfangplus@gmail.com>
2026-06-18 20:35:50 +0330

    Revert "Support old single-touch touchscreens with BTN_LEFT"
    This reverts commit 453ecfcc67342a24f02f917c5257f3baba38f3f8.

--- services/inputflinger/reader/mapper/accumulator/TouchButtonAccumulator.cpp
+++ services/inputflinger/reader/mapper/accumulator/TouchButtonAccumulator.cpp
@@ -22,7 +22,7 @@
 namespace android {
 
 void TouchButtonAccumulator::configure() {
-    mHaveBtnTouch = mDeviceContext.hasScanCode(BTN_TOUCH) || mDeviceContext.hasScanCode(BTN_LEFT);
+    mHaveBtnTouch = mDeviceContext.hasScanCode(BTN_TOUCH);
     mHaveStylus = mDeviceContext.hasScanCode(BTN_TOOL_PEN) ||
             mDeviceContext.hasScanCode(BTN_TOOL_RUBBER) ||
             mDeviceContext.hasScanCode(BTN_TOOL_BRUSH) ||
@@ -31,7 +31,7 @@ void TouchButtonAccumulator::configure() {
 }
 
 void TouchButtonAccumulator::reset() {
-    mBtnTouch = mDeviceContext.isKeyPressed(BTN_TOUCH) || mDeviceContext.isKeyPressed(BTN_LEFT);
+    mBtnTouch = mDeviceContext.isKeyPressed(BTN_TOUCH);
     mBtnStylus = mDeviceContext.isKeyPressed(BTN_STYLUS) ||
             mDeviceContext.isKeyCodePressed(AKEYCODE_STYLUS_BUTTON_PRIMARY);
     // BTN_0 is what gets mapped for the HID usage Digitizers.SecondaryBarrelSwitch
@@ -58,7 +58,6 @@ void TouchButtonAccumulator::process(const RawEvent& rawEvent) {
     if (rawEvent.type == EV_KEY) {
         switch (rawEvent.code) {
             case BTN_TOUCH:
-            case BTN_LEFT:
                 mBtnTouch = rawEvent.value;
                 break;
             case BTN_STYLUS:
